e5347f31f4bb9a7598625bbb7b325bfa147a227b
Alejandro610 <alejolavaca@gmail.com>
2025-02-16 18:44:18 +0100

    Add files via upload

---
+++ script_corrected (1).js
@@ -0,0 +1,7 @@
+
+document.getElementById('verDestinos').addEventListener('click', function() {
+    window.scrollTo({
+        top: document.getElementById('destinos').offsetTop,
+        behavior: 'smooth'
+    });
+});
